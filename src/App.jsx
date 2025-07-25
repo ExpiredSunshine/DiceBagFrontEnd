aedc867d4895a570a6c2c8de407368f4e9b7e09f
A.J. Croft <acroft41@yahoo.com>
2025-07-25 18:29:53 -0500

    create wip main section and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className="App-main">
-        <h2>Welcome to DiceBag</h2>
-        <p>True Random Dice Rolling</p>
-        {/* Dice rolling components will go here */}
-      </main>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <main className="main">
+      <h2>Welcome to DiceBag</h2>
+      <p>True Random Dice Rolling</p>
+      {/* Dice rolling components will go here */}
+    </main>
+  );
+}
+
+export default Main;
