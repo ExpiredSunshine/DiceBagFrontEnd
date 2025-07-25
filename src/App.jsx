c276b00004f1b2fc05440e7ed5bae44b01fa468d
A.J. Croft <acroft41@yahoo.com>
2025-07-25 18:53:31 -0500

    create roller route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './routes/Home/Home';
+import Roller from './components/Roller/Roller';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/roller" element={<Roller />} />
       </Routes>
     </div>
   );
---
+++ src/components/Roller/Roller.jsx
@@ -0,0 +1,11 @@
+import Main from '../Main/Main';
+
+function Roller() {
+  return (
+    <div className="roller">
+      <Main />
+    </div>
+  );
+}
+
+export default Roller;
