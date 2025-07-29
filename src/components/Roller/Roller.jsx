b525b073966578ea0196b733256cdaaf3d90665f
A.J. Croft <acroft41@yahoo.com>
2025-07-29 16:07:25 -0500

    create dice card section and initial styles

--- src/components/Roller/Roller.jsx
+++ src/components/Roller/Roller.jsx
@@ -1,7 +1,34 @@
+import './Roller.css';
+
 function Roller() {
   return (
-    <div className="roller">
-      <h2>WIP ROLL PAGE</h2>
+    <div className="main">
+      <div className="dice_card__section">
+        <div className="dice_cards_container">
+          <div className="dice_card">
+            <h3 className="dice_title">1D4</h3>
+          </div>
+          <div className="dice_card">
+            <h3 className="dice_title">1D6</h3>
+          </div>
+          <div className="dice_card">
+            <h3 className="dice_title">1D8</h3>
+          </div>
+          <div className="dice_card">
+            <h3 className="dice_title">1D10</h3>
+          </div>
+          <div className="dice_card">
+            <h3 className="dice_title">1D12</h3>
+          </div>
+          <div className="dice_card">
+            <h3 className="dice_title">1D20</h3>
+          </div>
+          <div className="dice_card">
+            <h3 className="dice_title">1D100</h3>
+          </div>
+        </div>
+        <button className="roll_button">roll all</button>
+      </div>
     </div>
   );
 }
